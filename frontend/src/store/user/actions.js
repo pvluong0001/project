5bce61cd36dceb0408d1b109c614f000306b3a56
pvluong0001 <pvluong0001@gmail.com>
2020-05-08 13:49:01 +0700

    update login logic in frontned

--- frontend/src/store/user/actions.js
+++ frontend/src/store/user/actions.js
@@ -1,13 +1,11 @@
 import { httpClient } from 'src/config/httpClient'
-import { SUCCESS_STATUS_CODE } from 'src/config/constant'
 
 export async function LOGIN ({ commit, dispatch }, payloads) {
   try {
     const response = await httpClient.post('/auth/login', payloads)
-
-    if (response.code === SUCCESS_STATUS_CODE) {
-      localStorage.setItem(process.env.TOKEN_NAME, response.result.token)
-      this._vm.$axios.defaults.headers.common.Authorization = `Bearer ${response.result.token}`
+    if (response.data.token) {
+      localStorage.setItem(process.env.TOKEN_NAME, response.data.token)
+      this._vm.$axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`
 
       commit('notify/setNotify', {
         color: 'teal',
@@ -19,7 +17,7 @@ export async function LOGIN ({ commit, dispatch }, payloads) {
 
       this.$router.replace('/admin')
     } else {
-      commit('setError', response.error || response.message)
+      commit('setError', response.errors || response.message)
     }
   } catch (e) {
     commit('setError', 'Login failed! Please contact admin.')
@@ -42,40 +40,13 @@ export async function LOGOUT ({ commit, state }) {
 }
 
 export async function loadUser ({ commit }) {
-  const response = await httpClient.get('/auth/get-user')
-
-  if (response.code === SUCCESS_STATUS_CODE) {
-    commit('setUser', response.result)
+  const response = await httpClient.get('/auth/user/info')
+  if (response.data) {
+    commit('setUser', response.data)
   } else {
     commit('notify/setNotify', {
       color: 'negative',
       message: 'Session invalid!'
     }, { root: true })
   }
-  // try {
-  //   switch (response.result.code) {
-  //     case 200:
-  //       commit('setUser', response.data)
-  //       break
-  //     case 600:
-  //       commit('notify/setNotify', {
-  //         color: 'negative',
-  //         message: 'Session invalid!'
-  //       }, { root: true })
-  //
-  //       localStorage.removeItem(process.env.TOKEN_NAME)
-  //       this.$router.replace('/login')
-  //       break
-  //     default:
-  //       commit('notify/setNotify', {
-  //         color: 'negative',
-  //         message: response.result.message
-  //       }, { root: true })
-  //   }
-  // } catch (e) {
-  //   commit('notify/setNotify', {
-  //     color: 'negative',
-  //     message: 'Cannot load user info'
-  //   }, { root: true })
-  // }
 }
