8393ff12fd597ab3e65273199b78ae88699322a8
pvluong0001 <pvluong0001@gmail.com>
2020-05-08 11:45:43 +0700

    set route explorer

--- api/src/helpers/base.helper.js
+++ api/src/helpers/base.helper.js
@@ -1,3 +1,26 @@
 export function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function fetchExplorerRoute(routers = {}, basePath = '/') {
+  return Object.keys(routers).reduce((result, item) => {
+    return {
+      ...result,
+      [item]: _convertRoute(routers[item], basePath)
+    } 
+  }, {})
+}
+
+function _convertRoute(router, basePath) {
+  let layers = router.stack;
+
+  return layers.filter(layer => layer.route).map(layer => {
+    const path = `${basePath}${layer.route.path}`.replace('//', '/');
+
+    return {
+      path,
+      method: Object.keys(layer.route.methods)[0].toUpperCase(),
+      url: `http://localhost:${process.env.PORT}/${path}`
+    }
+  })
+}
--- api/src/helpers/config-route.helper.js
+++ api/src/helpers/config-route.helper.js
@@ -1,5 +1,12 @@
 import {Router} from 'express';
 import authRoute from '@routes/auth.route';
+import { fetchExplorerRoute } from './base.helper';
+
+// const routeStack = authRoute.stack.filter(layer => {
+//   return layer.roter;
+// })
+// console.log(authRoute.stack, '====');
+
 
 export function configRouter(app) {
   const router = Router();
@@ -12,4 +19,14 @@ export function configRouter(app) {
 
   /** set prefix for all route */
   app.use('/api/v1', router);
+
+  if(process.env.NODE_ENV !== 'production') {
+    app.get('/api/v1/explorer', (req, res) => {
+      return res.json({
+        api: fetchExplorerRoute({
+          auth: authRoute
+        }, 'api/v1')
+      })
+    })
+  }
 }
